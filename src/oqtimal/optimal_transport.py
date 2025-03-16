2d6d28af87d090d1c3dc1d1d4128e6f9f8808e89
larskue <lars@kuehmichel.de>
2025-03-16 21:58:56 +0100

    fix tests

--- src/oqtimal/optimal_transport.py
+++ src/oqtimal/optimal_transport.py
@@ -1,5 +1,5 @@
 
 from oqtimal.algorithms import sinkhorn_knopp
 
-def optimal_transport(x, y, algorithm="simplex", cost="euclidean", **kwargs):
-    return sinkhorn_knopp(x, y, **kwargs)
+def optimal_transport(key, x, y, algorithm="simplex", cost="euclidean", **kwargs):
+    return sinkhorn_knopp(key, x, y, **kwargs)
--- tests/test_optimal_transport.py
+++ tests/test_optimal_transport.py
@@ -1,8 +1,13 @@
 
+import jax
+import jax.numpy as jnp
+
+
 def test_optimal_transport():
     from oqtimal import optimal_transport
 
-    x = [1, 2, 3]
-    y = [4, 5, 6]
+    key = jax.random.PRNGKey(42)
+    x = jnp.array([1, 2, 3])
+    y = jnp.array([4, 5, 6])
 
-    x, y = optimal_transport(x, y)
+    x, y = optimal_transport(key, x, y)
