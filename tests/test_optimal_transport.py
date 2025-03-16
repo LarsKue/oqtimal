067c6ffa2b9331f1b0410eaf866982ad4844ba4e
larskue <lars@kuehmichel.de>
2025-03-16 21:58:56 +0100

    fix tests

--- tests/test_optimal_transport.py
+++ tests/test_optimal_transport.py
@@ -1,8 +1,10 @@
 
+import jax.numpy as jnp
+
 def test_optimal_transport():
     from oqtimal import optimal_transport
 
-    x = [1, 2, 3]
-    y = [4, 5, 6]
+    x = jnp.array([1, 2, 3])
+    y = jnp.array([4, 5, 6])
 
     x, y = optimal_transport(x, y)
